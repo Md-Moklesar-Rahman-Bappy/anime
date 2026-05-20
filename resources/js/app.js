1382f3a9c8abf267f75505782be960e477e44b08
Md-Moklesar-Rahman-Bappy <risingbappy1@gmail.com>
2026-05-20 15:34:30 +0600

    feat: Enhance admin dashboard with new metrics and charts for user growth, anime types, and statuses; update session handling

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,9 +3,11 @@ import './bootstrap';
 import 'plyr/dist/plyr.css';
 import Alpine from 'alpinejs';
 import { player } from './components/player';
+import { dashboard } from './admin/dashboard';
 
 window.Alpine = Alpine;
 
 Alpine.data('player', player);
+Alpine.data('dashboard', dashboard);
 
 Alpine.start();
